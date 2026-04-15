ec2ae931e379bd4b431472fd35a4f5e62d8fa29c
Pacca <pacmax510@gmail.com>
2026-04-15 16:44:34 -0700

    Started adding platform support for EHZ

---
+++ LevelsS2/EHZ/Misc Object Data/Map - Floating Platform.asm
@@ -0,0 +1,16 @@
+Map_99dd:
+Map_99dd_0: 	dc.w Map_99dd_4-Map_99dd
+Map_99dd_2: 	dc.w Map_99dd_12-Map_99dd
+Map_99dd_4: 	dc.b $0, $2
+	dc.b $F4, $F, $0, $56, $FF, $E0
+	dc.b $F4, $F, $8, $56, $0, $0
+Map_99dd_12: 	dc.b $0, $8
+	dc.b $CC, $7, $0, $A, $FF, $E0
+	dc.b $CC, $D, $0, $12, $FF, $F0
+	dc.b $DC, $D, $0, $1A, $FF, $F0
+	dc.b $CC, $7, $0, $22, $0, $10
+	dc.b $EC, $F, $0, $2A, $FF, $E0
+	dc.b $EC, $F, $8, $2A, $0, $0
+	dc.b $C, $F, $0, $3A, $FF, $E0
+	dc.b $C, $F, $8, $3A, $0, $0
+	even
