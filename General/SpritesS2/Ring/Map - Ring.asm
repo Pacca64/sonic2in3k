84e9629a16fa4576eaef70cf7f4b07d29600249c
Pacca <pacmax510@gmail.com>
2026-04-22 21:26:06 -0700

    fixed ring art and some of the HUD art in Sonic 2 zones

---
+++ General/SpritesS2/Ring/Map - Ring.asm
@@ -0,0 +1,28 @@
+Map_9cf5:
+Map_9cf5_0: 	dc.w Map_9cf5_12-Map_9cf5
+Map_9cf5_2: 	dc.w Map_9cf5_1A-Map_9cf5
+Map_9cf5_4: 	dc.w Map_9cf5_22-Map_9cf5
+Map_9cf5_6: 	dc.w Map_9cf5_2A-Map_9cf5
+Map_9cf5_8: 	dc.w Map_9cf5_32-Map_9cf5
+Map_9cf5_A: 	dc.w Map_9cf5_3A-Map_9cf5
+Map_9cf5_C: 	dc.w Map_9cf5_42-Map_9cf5
+Map_9cf5_E: 	dc.w Map_9cf5_4A-Map_9cf5
+Map_9cf5_10: 	dc.w Map_9cf5_52-Map_9cf5
+Map_9cf5_12: 	dc.b $0, $1
+	dc.b $F8, $5, $0, $0, $FF, $F8
+Map_9cf5_1A: 	dc.b $0, $1
+	dc.b $F8, $5, $0, $4, $FF, $F8
+Map_9cf5_22: 	dc.b $0, $1
+	dc.b $F8, $1, $0, $8, $FF, $FC
+Map_9cf5_2A: 	dc.b $0, $1
+	dc.b $F8, $5, $8, $4, $FF, $F8
+Map_9cf5_32: 	dc.b $0, $1
+	dc.b $F8, $5, $0, $A, $FF, $F8
+Map_9cf5_3A: 	dc.b $0, $1
+	dc.b $F8, $5, $18, $A, $FF, $F8
+Map_9cf5_42: 	dc.b $0, $1
+	dc.b $F8, $5, $8, $A, $FF, $F8
+Map_9cf5_4A: 	dc.b $0, $1
+	dc.b $F8, $5, $10, $A, $FF, $F8
+Map_9cf5_52: 	dc.b $0, $0
+	even
