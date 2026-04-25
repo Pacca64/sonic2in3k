6c84e7b443e99a601c4c2c438fce6542ba04eafb
Pacca <pacmax510@gmail.com>
2026-04-25 14:18:21 -0700

    Implemented Emerald Hill Zones PLCs

---
+++ LevelsS2/EHZ/Misc Object Data/Map - Coconuts.asm
@@ -0,0 +1,24 @@
+Map_d7a5:
+Map_d7a5_0: 	dc.w Map_d7a5_8-Map_d7a5
+Map_d7a5_2: 	dc.w Map_d7a5_22-Map_d7a5
+Map_d7a5_4: 	dc.w Map_d7a5_3C-Map_d7a5
+Map_d7a5_6: 	dc.w Map_d7a5_56-Map_d7a5
+Map_d7a5_8: 	dc.b $0, $4
+	dc.b $0, $5, $0, $1A, $FF, $FE
+	dc.b $F0, $9, $0, $0, $FF, $FC
+	dc.b $0, $D, $0, $6, $FF, $F4
+	dc.b $10, $1, $0, $E, $0, $C
+Map_d7a5_22: 	dc.b $0, $4
+	dc.b $0, $5, $0, $1E, $FF, $FE
+	dc.b $F0, $9, $0, $0, $FF, $FC
+	dc.b $0, $D, $0, $10, $FF, $F4
+	dc.b $10, $1, $0, $18, $0, $C
+Map_d7a5_3C: 	dc.b $0, $4
+	dc.b $F8, $1, $0, $22, $0, $7
+	dc.b $F0, $9, $0, $0, $FF, $FC
+	dc.b $0, $D, $0, $10, $FF, $F4
+	dc.b $10, $1, $0, $18, $0, $C
+Map_d7a5_56: 	dc.b $0, $2
+	dc.b $F8, $1, $40, $24, $FF, $F8
+	dc.b $F8, $1, $48, $24, $0, $0
+	even
---
+++ LevelsS2/EHZ/Misc Object Data/Map - Tension Bridge.asm
@@ -0,0 +1,8 @@
+Map_e00d:
+Map_e00d_0: 	dc.w Map_e00d_4-Map_e00d
+Map_e00d_2: 	dc.w Map_e00d_C-Map_e00d
+Map_e00d_4: 	dc.b $0, $1
+	dc.b $F8, $5, $0, $4, $FF, $F8
+Map_e00d_C: 	dc.b $0, $1
+	dc.b $F8, $5, $0, $0, $FF, $F8
+	even
