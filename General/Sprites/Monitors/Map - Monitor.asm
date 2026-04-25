4eefdd7cd90318f955343a919768e7350c38d400
Pacca <pacmax510@gmail.com>
2026-04-24 18:49:46 -0700

    Fixed ring monitor art for Sonic 2 zones

--- General/Sprites/Monitors/Map - Monitor.asm
+++ General/Sprites/Monitors/Map - Monitor.asm
@@ -1,47 +1,48 @@
-Map_Monitor_:
-		dc.w word_1DBBA-Map_Monitor_
-		dc.w word_1DBC2-Map_Monitor_
-		dc.w word_1DBD0-Map_Monitor_
-		dc.w word_1DBDE-Map_Monitor_
-		dc.w word_1DBEC-Map_Monitor_
-		dc.w word_1DBFA-Map_Monitor_
-		dc.w word_1DC08-Map_Monitor_
-		dc.w word_1DC16-Map_Monitor_
-		dc.w word_1DC24-Map_Monitor_
-		dc.w word_1DC32-Map_Monitor_
-		dc.w word_1DC40-Map_Monitor_
-		dc.w word_1DC4E-Map_Monitor_
-word_1DBBA:	dc.w 1
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBC2:	dc.w 2
-		dc.b  $F3,   5,   0, $18, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBD0:	dc.w 2
-		dc.b  $F3,   5,   3, $10, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBDE:	dc.w 2
-		dc.b  $F3,   5,   0, $1C, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBEC:	dc.w 2
-		dc.b  $F3,   5, $20, $20, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBFA:	dc.w 2
-		dc.b  $F3,   5,   0, $24, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC08:	dc.w 2
-		dc.b  $F3,   5,   0, $30, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC16:	dc.w 2
-		dc.b  $F3,   5,   0, $2C, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC24:	dc.w 2
-		dc.b  $F3,   5,   0, $34, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC32:	dc.w 2
-		dc.b  $F3,   5,   0, $28, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC40:	dc.w 2
-		dc.b  $F3,   5,   0, $38, $FF, $F8
-		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC4E:	dc.w 1
-		dc.b    0,  $D,   0, $10, $FF, $F0
+Map_4e75:
+Map_4e75_0: 	dc.w Map_4e75_18-Map_4e75
+Map_4e75_2: 	dc.w Map_4e75_20-Map_4e75
+Map_4e75_4: 	dc.w Map_4e75_2E-Map_4e75
+Map_4e75_6: 	dc.w Map_4e75_3C-Map_4e75
+Map_4e75_8: 	dc.w Map_4e75_4A-Map_4e75
+Map_4e75_A: 	dc.w Map_4e75_58-Map_4e75
+Map_4e75_C: 	dc.w Map_4e75_66-Map_4e75
+Map_4e75_E: 	dc.w Map_4e75_74-Map_4e75
+Map_4e75_10: 	dc.w Map_4e75_82-Map_4e75
+Map_4e75_12: 	dc.w Map_4e75_90-Map_4e75
+Map_4e75_14: 	dc.w Map_4e75_9E-Map_4e75
+Map_4e75_16: 	dc.w Map_4e75_AC-Map_4e75
+Map_4e75_18: 	dc.b $0, $1
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_20: 	dc.b $0, $2
+	dc.b $F3, $5, $0, $18, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_2E: 	dc.b $0, $2
+	dc.b $F3, $5, $3, $10, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_3C: 	dc.b $0, $2
+	dc.b $F3, $5, $0, $1C, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_4A: 	dc.b $0, $2
+	dc.b $F3, $5, $20, $20, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_58: 	dc.b $0, $2
+	dc.b $F3, $5, $0, $24, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_66: 	dc.b $0, $2
+	dc.b $F3, $5, $0, $30, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_74: 	dc.b $0, $2
+	dc.b $F3, $5, $0, $2C, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_82: 	dc.b $0, $2
+	dc.b $F3, $5, $0, $34, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_90: 	dc.b $0, $2
+	dc.b $F3, $5, $0, $28, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_9E: 	dc.b $0, $2
+	dc.b $F3, $5, $0, $38, $FF, $F8
+	dc.b $F0, $F, $0, $0, $FF, $F0
+Map_4e75_AC: 	dc.b $0, $1
+	dc.b $0, $D, $0, $10, $FF, $F0
+	even
