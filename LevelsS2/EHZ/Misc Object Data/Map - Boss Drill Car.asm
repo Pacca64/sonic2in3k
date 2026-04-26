11249243d37be490e27b4f8ae63d1ce78ef8edec
Pacca <pacmax510@gmail.com>
2026-04-26 02:16:57 -0700

    Fixed EHZ boss art. EHZ Boss seems fully supported now!

--- LevelsS2/EHZ/Misc Object Data/Map - Boss Drill Car.asm
+++ LevelsS2/EHZ/Misc Object Data/Map - Boss Drill Car.asm
@@ -1,28 +1,28 @@
-Map_1dc9:
-Map_1dc9_0: 	dc.w Map_1dc9_10-Map_1dc9
-Map_1dc9_2: 	dc.w Map_1dc9_24-Map_1dc9
-Map_1dc9_4: 	dc.w Map_1dc9_2C-Map_1dc9
-Map_1dc9_6: 	dc.w Map_1dc9_34-Map_1dc9
-Map_1dc9_8: 	dc.w Map_1dc9_3C-Map_1dc9
-Map_1dc9_A: 	dc.w Map_1dc9_44-Map_1dc9
-Map_1dc9_C: 	dc.w Map_1dc9_4C-Map_1dc9
-Map_1dc9_E: 	dc.w Map_1dc9_54-Map_1dc9
-Map_1dc9_10: 	dc.b $0, $3
+Map_c830:
+Map_c830_0: 	dc.w Map_c830_10-Map_c830
+Map_c830_2: 	dc.w Map_c830_24-Map_c830
+Map_c830_4: 	dc.w Map_c830_2C-Map_c830
+Map_c830_6: 	dc.w Map_c830_34-Map_c830
+Map_c830_8: 	dc.w Map_c830_3C-Map_c830
+Map_c830_A: 	dc.w Map_c830_44-Map_c830
+Map_c830_C: 	dc.w Map_c830_4C-Map_c830
+Map_c830_E: 	dc.w Map_c830_54-Map_c830
+Map_c830_10: 	dc.b $0, $3
 	dc.b $F0, $F, $0, $0, $FF, $D0
 	dc.b $F0, $F, $0, $10, $FF, $F0
 	dc.b $F0, $F, $0, $20, $0, $10
-Map_1dc9_24: 	dc.b $0, $1
+Map_c830_24: 	dc.b $0, $1
 	dc.b $F0, $F, $0, $30, $FF, $F0
-Map_1dc9_2C: 	dc.b $0, $1
+Map_c830_2C: 	dc.b $0, $1
 	dc.b $F0, $F, $0, $40, $FF, $F0
-Map_1dc9_34: 	dc.b $0, $1
+Map_c830_34: 	dc.b $0, $1
 	dc.b $F0, $F, $0, $50, $FF, $F0
-Map_1dc9_3C: 	dc.b $0, $1
+Map_c830_3C: 	dc.b $0, $1
 	dc.b $F0, $F, $0, $60, $FF, $F0
-Map_1dc9_44: 	dc.b $0, $1
+Map_c830_44: 	dc.b $0, $1
 	dc.b $F0, $F, $10, $60, $FF, $F0
-Map_1dc9_4C: 	dc.b $0, $1
+Map_c830_4C: 	dc.b $0, $1
 	dc.b $F0, $F, $0, $70, $FF, $F0
-Map_1dc9_54: 	dc.b $0, $1
+Map_c830_54: 	dc.b $0, $1
 	dc.b $F0, $F, $10, $70, $FF, $F0
 	even
