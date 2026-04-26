f422172be3e0f9083302637648990f3c3bfb41d9
Pacca <pacmax510@gmail.com>
2026-04-25 20:23:37 -0700

    Filled in the EHZ debug list

--- LevelsS2/EHZ/Debug/Main.asm
+++ LevelsS2/EHZ/Debug/Main.asm
@@ -1,3 +1,26 @@
 	;          Object			Mappings			Subtype Frame Arttile
 	dbglistobj Obj_Ring,			Map_Ring,			   0,   0, make_art_tile(ArtTile_Ring,1,1)
 	dbglistobj Obj_Monitor,			Map_Monitor,			   6,   0, make_art_tile(ArtTile_Monitors,0,0)
+
+	dbglistobj Obj_PathSwap,		Map_PathSwap,			   9,   1, make_art_tile(ArtTile_Ring,1,0)
+	dbglistobj Obj_PathSwap,		Map_PathSwap,			  $D,   5, make_art_tile(ArtTile_Ring,1,0)
+
+	dbglistobj Obj_EHZ_Waterfall,	Obj49_MapUnc_20C50,		  0,   0, make_art_tile(ArtTile_ArtNem_Waterfall,1,0)
+	dbglistobj Obj_EHZ_Waterfall,	Obj49_MapUnc_20C50,		  2,   3, make_art_tile(ArtTile_ArtNem_Waterfall,1,0)
+	dbglistobj Obj_EHZ_Waterfall,	Obj49_MapUnc_20C50,		  4,   5, make_art_tile(ArtTile_ArtNem_Waterfall,1,0)
+
+	dbglistobj Obj_FloatingPlatform, Map_EHZFloatingPlatform,	1,   0, make_art_tile($000,2,0)
+	dbglistobj Obj_FloatingPlatform, Map_EHZFloatingPlatform,	$9A,   1, make_art_tile($000,2,0)
+
+	dbglistobj Obj_Spikes, Map_Spikes,	0,   0, make_art_tile(ArtTile_SpikesSprings+$8,0,0)
+
+	dbglistobj Obj_Spring,			Map_Spring,			 $81,   0, make_art_tile(ArtTile_SpikesSprings+$10,0,0)
+	dbglistobj Obj_Spring,			Map_Spring,			 $90,   3, make_art_tile(ArtTile_SpikesSprings+$20,0,0)
+	dbglistobj Obj_Spring,			Map_Spring,			 $A0,   6, make_art_tile(ArtTile_SpikesSprings+$10,0,0)
+
+	dbglistobj Obj_Spring,			Map_Spring,			 $30,   7, make_art_tile(ArtTile_DiagonalSpring,0,0)
+	dbglistobj Obj_Spring,			Map_Spring,			 $40,  $A, make_art_tile(ArtTile_DiagonalSpring,0,0)
+
+	dbglistobj Obj_Buzzer, Obj4B_MapUnc_2D2EA,   0,   0, make_art_tile(ArtTile_ArtNem_Buzzer,0,0)
+	dbglistobj Obj_Masher, Obj5C_MapUnc_2D442,   0,   0, make_art_tile(ArtTile_ArtNem_Masher,0,0)
+	dbglistobj Obj_Coconuts, Obj9D_Obj98_MapUnc_37D96, $1E,   0, make_art_tile(ArtTile_ArtNem_Coconuts,0,0)
