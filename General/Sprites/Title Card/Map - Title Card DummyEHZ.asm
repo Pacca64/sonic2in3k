a4f045f20a03045a2d3a67eb17d8d97a044f8878
Pacca <pacmax510@gmail.com>
2026-04-25 18:01:32 -0700

    Fixed Emerald Hill Zone title card

---
+++ General/Sprites/Title Card/Map - Title Card DummyEHZ.asm
@@ -0,0 +1,18 @@
+Map_bff9:
+Map_bff9_0: 	dc.w Map_bff9_6-Map_bff9
+Map_bff9_2: 	dc.w Map_bff9_38-Map_bff9
+Map_bff9_4: 	dc.w Map_bff9_40-Map_bff9
+Map_bff9_6: 	dc.b $0, $8
+	dc.b $F5, $A, $0, $0, $FF, $B0
+	dc.b $F5, $6, $0, $9, $FF, $D8
+	dc.b $F5, $E, $0, $F, $FF, $E8
+	dc.b $F5, $2, $0, $1B, $0, $8
+	dc.b $F5, $6, $0, $1E, $0, $18
+	dc.b $F5, $2, $0, $24, $0, $28
+	dc.b $F5, $2, $0, $15, $0, $30
+	dc.b $F5, $2, $0, $15, $0, $38
+Map_bff9_38: 	dc.b $0, $1
+	dc.b $F4, $6, $0, $1E, $FF, $F8
+Map_bff9_40: 	dc.b $0, $1
+	dc.b $F2, $2, $0, $24, $FF, $F5
+	even
